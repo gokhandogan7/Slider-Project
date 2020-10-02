41530aa9309478bd1dbd31d65950c2f64abe60ea
gokhan <gdogan3746@gmail.com>
2020-10-02 22:20:23 +0200

    lessons object has created in js file

--- script.js
+++ script.js
@@ -1,2 +1,33 @@
-
+var lessons = [
+    {
+      name: "HTML",
+      image : "./img/html.jpg",
+      link : "https://lms.clarusway.com/course/view.php?id=3"
+    },
+    {
+      name: "CSS",
+      image : "./img/css.jpeg",
+      link : "https://lms.clarusway.com/course/view.php?id=6"
+    },
+    {
+      name: "JS",
+      image : "./img/js.jpg",
+      link : "https://lms.clarusway.com/course/view.php?id=18"
+    },
+    {
+      name: "REACT",
+      image : "./img/react.jpg",
+      link : "https://lms.clarusway.com/course/view.php?id=49"
+    },
+    {
+      name: "REACT NATIVE",
+      image : "./img/react-native.jpg",
+      link : "https://lms.clarusway.com/course/view.php?id=54"
+    },
+    {
+      name: "DJANGO",
+      image : "./img/djengo.jpg",
+      link : "https://lms.clarusway.com/course/view.php?id=53"
+    }
+]
 
