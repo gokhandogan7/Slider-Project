8df089c25c083abb57cbe40e3757e1f56b7aaee8
gokhan <gdogan3746@gmail.com>
2020-10-02 22:04:28 +0200

    created project files

---
+++ script1.js
@@ -0,0 +1,73 @@
+var lessons = [
+    {
+      name: "HTML",
+      image : "./img/html.jpg",
+      link : "https://lms.clarusway.com/course/view.php?id=3"
+    },
+    {
+      name: "CSS",
+      image : "./img/css.jpeg",
+      link : "https://lms.clarusway.com/course/view.php?id=6"
+    },
+    {
+      name: "JS",
+      image : "./img/js.jpg",
+      link : "https://lms.clarusway.com/course/view.php?id=18"
+    },
+    {
+      name: "REACT",
+      image : "./img/react.jpg",
+      link : "https://lms.clarusway.com/course/view.php?id=49"
+    },
+    {
+      name: "REACT NATIVE",
+      image : "./img/react-native.jpg",
+      link : "https://lms.clarusway.com/course/view.php?id=54"
+    },
+    {
+      name: "DJANGO",
+      image : "./img/djengo.jpg",
+      link : "https://lms.clarusway.com/course/view.php?id=53"
+    }
+]
+
+var index = 2;
+var countIndex = lessons.length;
+const image = document.querySelector(".lesson-image");
+const title = document.querySelector(".lesson-title");
+const link = document.querySelector(".lesson-link");
+const leftButton = document.querySelector(".button-left");
+const rightButton = document.querySelector(".button-right");
+
+showSlide(index);
+
+leftButton.addEventListener("click", function(){
+    index--
+    if (index<0){
+        index = countIndex-1;
+    }
+    showSlide(index)
+    console.log(index)
+    
+})
+rightButton.addEventListener("click", function(){
+    index++
+    if (index>lessons.length-1){
+        index = 0;
+    }
+    showSlide(index)
+    console.log(index)
+    
+})
+
+
+
+function showSlide(index){
+
+    
+
+    image.setAttribute('src', lessons[index].image);
+    title.innerText = (lessons[index].name)
+    link.setAttribute('href',lessons[index].link);
+
+}
